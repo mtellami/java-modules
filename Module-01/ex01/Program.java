9bc40844bb33c8571abcd31151c8d5481b6d6799
mtellami <moadtell@gmail.com>
2024-02-06 17:07:34 +0100

    module01

--- Module-01/ex01/Program.java
+++ Module-01/ex01/Program.java
@@ -1,4 +1,11 @@
 public class Program {
 	public static void main(String[] args) {
+		User user1 = new User("user1", 100);
+		User user2 = new User("user2", 200);
+		User user3 = new User("user3", 300);
+
+		user1.printConsole();
+		user2.printConsole();
+		user3.printConsole();
 	}
 };
