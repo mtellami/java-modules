56cd5e0274f14311df7f17b3ec4f3f1669e96596
mtellami <moadtell@gmail.com>
2024-02-08 11:02:40 +0100

    stop again..

--- Module-01/ex04/TransactionsService.java
+++ Module-01/ex04/TransactionsService.java
@@ -1,2 +1,11 @@
 public class TransactionsService {
+	private UsersList userDatabase;
+
+	TransactionsService() {
+		userDatabase = new UsersArrayList();
+	}
+
+	public void addUser(User user) {
+		userDatabase.addUser(user);
+	}
 };
